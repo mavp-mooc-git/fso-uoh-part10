e8aa78af977afac67898cd64cfac454fcb88d6e8
MAVP <localhost@debian>
2021-12-04 11:35:49 -0300

    part10-11

---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,39 @@
+/*import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    // Replace the IP address part with your own IP address!
+    const response = await fetch('http://192.168.100.10:5000/api/repositories');
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;*/
+
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+    // Other options
+  });
+
+  return { data, loading, error };
+};
+
+export default useRepositories;
